7c5526659818e75081f3315672ff10d788e91660
Sam Nguyen <h.phuoc137@gmail.com>
2023-12-16 17:04:08 -0800

    fix header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,14 +2,20 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="bg-slate-200 shadow-sm">
-      <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-        <span className="text-slate-500">My</span>
-        <span className="text-slate-700">Estate</span>
-      </h1>
-      <form>
-        <input type="text" placeholder="Search..." />
-      </form>
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-slate-500">My</span>
+          <span className="text-slate-700">Estate</span>
+        </h1>
+        <form className="bg-slate-100 p-3 rounded-lg">
+          <input
+            className="bg-transparent"
+            type="text"
+            placeholder="Search..."
+          />
+        </form>
+      </div>
     </header>
   );
 }
