8017bac6b374242e3d9210ed6d38a8e4568410dd
Sam Nguyen <h.phuoc137@gmail.com>
2023-12-17 16:56:37 -0800

    create api for authentication signup

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose, { mongo } from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -14,6 +15,7 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 app.listen(port, () => {
@@ -23,3 +25,4 @@ app.listen(port, () => {
 // req is data from the browser/client
 // res is the response from the server side
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
