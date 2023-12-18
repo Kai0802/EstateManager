6d505a91c66f1788cefb1b53244ea54a01896f95
Sam Nguyen <h.phuoc137@gmail.com>
2023-12-18 15:47:20 -0800

    finish UI for sign up form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,37 +4,53 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-slate-400 slate shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-700">My</span>
-          <span className="text-slate-900">Estate</span>
+    <header className="bg-indigo-600 text-white shadow-lg">
+      <div className="container mx-auto flex justify-between items-center p-4">
+        <h1 className="text-2xl font-semibold tracking-tight">
+          <span className="text-indigo-300">My</span>
+          <span className="text-purple-300">Estate</span>
         </h1>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form className="relative">
           <input
-            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            className="appearance-none bg-white bg-opacity-20 rounded-full py-2 pl-4 pr-10 w-full sm:w-auto focus:outline-none focus:bg-opacity-100 focus:ring-2 focus:ring-purple-300 transition duration-200 text-gray-700 placeholder-white-800"
             type="text"
-            placeholder="Search..."
+            placeholder="Search properties..."
           />
-          <FaSearch className="text-slate-600" />
+          <button
+            type="submit"
+            className="absolute inset-y-0 right-0 flex items-center pr-3"
+          >
+            <FaSearch className="text-gray-500 hover:text-gray-700 transition duration-200" />
+          </button>
         </form>
-        <ul className="flex items-center gap-4">
-          <Link to="/">
-            <li className="text-slate-800 hidden sm:inline hover:underline">
-              Home
+        <nav>
+          <ul className="flex items-center gap-6 text-sm font-medium">
+            <li>
+              <Link
+                to="/"
+                className="hover:text-indigo-300 transition duration-300"
+              >
+                Home
+              </Link>
             </li>
-          </Link>
-
-          <Link to="/about">
-            <li className="text-slate-800 hidden sm:inline hover:underline">
-              About
+            <li>
+              <Link
+                to="/about"
+                className="hover:text-indigo-300 transition duration-300"
+              >
+                About
+              </Link>
             </li>
-          </Link>
-
-          <Link to="/sign-in">
-            <li className="text-slate-800 hover:underline">Sign in</li>
-          </Link>
-        </ul>
+            <li>
+              <Link
+                to="/sign-in"
+                className="hover:text-indigo-300 transition duration-300"
+              >
+                Sign in
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
--- client/src/views/SignUp.jsx
+++ client/src/views/SignUp.jsx
@@ -1,5 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen ">
+      <div className="w-full sm:max-w-md px-6 py-8 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <h1 className="text-3xl text-center font-semibold text-gray-700 my-7">
+          Sign Up
+        </h1>
+        <form className="space-y-6" action="#" method="POST">
+          <input
+            className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            type="text"
+            placeholder="username"
+            id="username"
+            name="username"
+            required
+          />
+          <input
+            className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            type="email"
+            placeholder="email"
+            id="email"
+            name="email"
+            required
+          />
+          <input
+            className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            type="password"
+            placeholder="password"
+            id="password"
+            name="password"
+            required
+          />
+          <button
+            className="w-full bg-indigo-700 text-white font-bold py-2 px-4 rounded-lg hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </form>
+        <div className="flex gap-1 mt-6 text-center">
+          <p className="text-gray-600">Have an account?</p>
+          <Link to="/sign-in" className="text-indigo-500 hover:text-indigo-600">
+            Sign In
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
