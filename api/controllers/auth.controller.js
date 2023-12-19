b818717ad71825acf27e54017eeba83c24f58b93
Sam Nguyen <h.phuoc137@gmail.com>
2023-12-18 23:49:48 -0800

    remove password from returning json

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -38,10 +38,11 @@ export const signin = async (req, res, next) => {
       const validPassword = bcrypt.compareSync(password, validUser.password);
       if (!validPassword) return next(errorHandler(401, "Wrong credential!"));
       const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...restUserInfo } = validUser._doc;
       res
         .cookie("access_token", token, { httpOnly: true })
         .status(200)
-        .json(validUser); // httpOnly: true make our cookie safer
+        .json(restUserInfo); // httpOnly: true make our cookie safer
     }
   } catch (error) {
     next(error);
