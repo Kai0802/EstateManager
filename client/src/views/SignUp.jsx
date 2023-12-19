8bffaa9dfca571cce7e3117d76060f84e481f8f7
Sam Nguyen <h.phuoc137@gmail.com>
2023-12-18 23:14:09 -0800

    add sign up functionality and errors handling

--- client/src/views/SignUp.jsx
+++ client/src/views/SignUp.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   function handleChange(event) {
     setFormData({
       ...formData,
@@ -14,19 +17,34 @@ export default function SignUp() {
   async function handleSubmit(event) {
     // event.preventDefault will prevent the page from refreshing when
     // we press on the submit button
-    event.preventDefault();
-    const res = await fetch("/api/auth/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    console.log(data);
+
+    try {
+      event.preventDefault();
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
   }
 
-  console.log(formData); // all of the information from the form is saved inside of formData
+  //console.log(formData); // all of the information from the form is saved inside of formData
   // after that we want to save it into the database
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -53,8 +71,11 @@ export default function SignUp() {
           id="password"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -63,6 +84,7 @@ export default function SignUp() {
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
