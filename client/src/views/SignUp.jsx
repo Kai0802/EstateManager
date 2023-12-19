a1d960f01fbe5e32b16eba132e43a8d5fb6ba19e
Sam Nguyen <h.phuoc137@gmail.com>
2023-12-18 22:33:35 -0800

    setup useState for submitting data from frontend to backend

--- client/src/views/SignUp.jsx
+++ client/src/views/SignUp.jsx
@@ -1,28 +1,47 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    });
+  }
+  console.log(formData); // all of the information from the form is saved inside of formData
+  // after that we want to save it into the database
+
+  function handleSubmit(event) {
+    // event.preventDefault will prevent the page from refreshing when
+    // we press on the submit button
+    event.preventDefault();
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
           Sign Up
