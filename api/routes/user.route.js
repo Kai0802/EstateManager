8206dad0e946b341858174edc98ec7f323bcbffb
Sam Nguyen <h.phuoc137@gmail.com>
2023-12-22 10:13:06 -0800

    create user update api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { test } from "../controllers/user.controller.js";
-
+import { updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.get("/test", test);
+// for updating user, we have extra checking, we need to check
+// if the person is authenticated or not by using the cookies,
+// so we will know which user we are updating.
+// when creating the user, we created a token, now we can use that
+// token to verify the user for updating user's information
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;
