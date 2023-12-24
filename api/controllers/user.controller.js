8f5eea93a4dd78b50d78397bc8b08ec3028137a3
Sam Nguyen <h.phuoc137@gmail.com>
2023-12-23 20:35:09 -0800

    add get user's listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcrypt from "bcrypt";
 export const test = (req, res) => {
@@ -48,3 +49,18 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export async function getUserListing(req, res, next) {
+  if (req.user.id !== req.params.id) {
+    return next(
+      errorHandler(401, "You can only view listing of your own account!")
+    );
+  } else {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { test } from "../controllers/user.controller.js";
-import { updateUser, deleteUser } from "../controllers/user.controller.js";
+import {
+  updateUser,
+  deleteUser,
+  getUserListing,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+
 const router = express.Router();
 
 router.get("/test", test);
@@ -12,4 +17,5 @@ router.get("/test", test);
 // token to verify the user for updating user's information
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListing);
 export default router;
