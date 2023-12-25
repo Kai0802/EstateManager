fe07d7344f507cef3077d12eff79fe20f0acbf50
Sam Nguyen <h.phuoc137@gmail.com>
2023-12-24 18:56:40 -0800

    add search functionality for header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-
+import { useNavigate } from "react-router-dom";
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  function handleSubmit(e) {
+    e.preventDefault();
+    // we want to get the query from the urls, when we add the search terms,
+    // all the default type will also be applied to the url as well. Therefore,
+    // we need to keep those queries even when we change the search term.
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`search?${searchQuery}`);
+  }
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-slate-400 slate shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -12,13 +31,20 @@ export default function Header() {
           <span className="text-slate-700">My</span>
           <span className="text-slate-900">Estate</span>
         </h1>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
+            onChange={(e) => setSearchTerm(e.target.value)}
             className="bg-transparent focus:outline-none w-24 sm:w-64"
             type="text"
             placeholder="Search..."
+            value={searchTerm}
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <ul className="flex items-center gap-4">
           <Link to="/">
